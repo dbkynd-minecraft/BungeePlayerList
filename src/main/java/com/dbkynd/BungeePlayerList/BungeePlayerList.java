bbfe5045a1263e41bb5311759afd9fe925b556ef
DBKynd <db@dbkynd.com>
2020-01-25 21:35:16 -0700

    manual bstats class

--- src/main/java/com/dbkynd/BungeePlayerList/BungeePlayerList.java
+++ src/main/java/com/dbkynd/BungeePlayerList/BungeePlayerList.java
@@ -1,14 +1,14 @@
 package com.dbkynd.bungeeplayerlist;
 
+import com.dbkynd.bungeeplayerlist.bstats.Metrics;
 import com.dbkynd.bungeeplayerlist.listeners.ProxyPingListener;
 import net.md_5.bungee.api.plugin.Plugin;
-import org.bstats.bungeecord.Metrics;
 
 public final class BungeePlayerList extends Plugin {
 
     @Override
     public void onEnable() {
-        new Metrics(this);
+        new Metrics(this, 6304);
         getProxy().getPluginManager().registerListener(this, new ProxyPingListener(this));
     }
 }
